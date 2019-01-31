eb2dc66d034722c4a330d9a3f86748396302def5
Elias Periañez <eliasperianez@gmail.com>
2019-01-31 22:04:56 +0100

    Changes on classes
    Created Action, Casilla, Turn and Unit.
    Deleted Board and changed to table propierty on Game

--- src/Casilla.java
+++ src/Casilla.java
@@ -1,29 +1,20 @@
-import javafx.util.Pair;
-
 /*
  *Creado por Elias Periañez
- *29 ene. 2019
+ *31 ene. 2019
  *Como parte del proyecto GameLib
  *Este archivo esta bajo la licencia de Creative Commons Reconocimiento 4.0 Internacional (Más informacion https://creativecommons.org/licenses/by/4.0/)
 ________________________________________________________________________________________________________________________________________________________
  *Created by Elias Periañez
- *29 ene. 2019
+ *31 ene. 2019
  *As part of the project GameLib
  *This file is under the Creative Commons Attribution 4.0 International (More info here https://creativecommons.org/licenses/by/4.0/)
  */
 
-public class Game {
-	
+public class Casilla {
 	/**
 	 * @author Elias Periañez
-	 * The table is the field where your game happend
+	 * This is the parametre that show wich unit is on it
 	 */
-	private Casilla [][] table;
-	
-	/**
-	 * @author Elias Periañez
-	 * Used Pair to represent the 2 possible players and their respective turns
-	 */
-	private Pair<Turn, Turn> turn;
+	private Unit unitOnIt = null;
 	
 }
